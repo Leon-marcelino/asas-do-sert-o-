7b9818e7dc70d6ff2da55160877874981e2b0dfd
Leon Marcelino <leon.marcelino@ufrpe.br>
2026-06-18 11:12:11 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ LARGURA_TELA = 400
 ALTURA_TELA = 600
 FPS = 60
 
-# Cores (RGB)
+# Cores
 AZUL_CEU = (113, 197, 207)
 AMARELO_PASSARO = (247, 219, 79)
 VERDE_CANO = (115, 191, 46)
@@ -11,16 +11,15 @@ BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 CHAO_COR = (222, 216, 149)
 
-# Arquivos de Recursos (Assets) do Gameplay
+# recursos
 ARQUIVO_FUNDO = "copilot_image_1781660572106.jpeg"
 ARQUIVO_MUSICA = "A_Morte_do_Vaqueiro_-_Mangaio_Edit_instrumental_KLICKAUD.mp3"
 
-# Arquivos do Menu Principal (Sem o botão de música)
+# menu
 ARQUIVO_MENU_FUNDO = "menu.png"
 ARQUIVO_BTN_JOGAR = "jogar.png"
 ARQUIVO_BTN_SAIR = "Copilot_20260617_195706.png"
 
-# Novas imagens da tela de Game Over
 ARQUIVO_BTN_REINICIAR = "reiniciar.png"
 ARQUIVO_BTN_VOLTAR_MENU = "Copilot_20260617_210409.png"
 ARQUIVO_CHAO = "chao.jpeg"
