722845675257e8de1fca49f98054ecfd189c7ec7
Leon Marcelino <leon.marcelino@ufrpe.br>
2026-06-27 00:24:15 -0300

    Atualizar o config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ LARGURA_TELA = 400
 ALTURA_TELA = 600
 FPS = 60
 
-# Cores
+# Cores (RGB)
 AZUL_CEU = (113, 197, 207)
 AMARELO_PASSARO = (247, 219, 79)
 VERDE_CANO = (115, 191, 46)
@@ -11,15 +11,56 @@ BRANCO = (255, 255, 255)
 PRETO = (0, 0, 0)
 CHAO_COR = (222, 216, 149)
 
-# recursos
-ARQUIVO_FUNDO = "copilot_image_1781660572106.jpeg"
-ARQUIVO_MUSICA = "A_Morte_do_Vaqueiro_-_Mangaio_Edit_instrumental_KLICKAUD.mp3"
-
-# menu
+# Arquivos do Menu Principal
 ARQUIVO_MENU_FUNDO = "menu.png"
 ARQUIVO_BTN_JOGAR = "jogar.png"
-ARQUIVO_BTN_SAIR = "Copilot_20260617_195706.png"
-
+ARQUIVO_BTN_SAIR = "sair.png"
 ARQUIVO_BTN_REINICIAR = "reiniciar.png"
-ARQUIVO_BTN_VOLTAR_MENU = "Copilot_20260617_210409.png"
-ARQUIVO_CHAO = "chao.jpeg"
+ARQUIVO_GAME_OVER = "game_over.png"
+ARQUIVO_FUNDO_NIVEIS = "fundo2.jpeg"
+
+# Imagens dos Botões de Fases/Níveis
+ARQUIVO_BTN_1 = "1.png"
+ARQUIVO_BTN_2 = "2.png"
+ARQUIVO_BTN_3 = "3.png"
+ARQUIVO_BTN_4 = "4.png"
+
+# ==========================================
+# ASSETS NÍVEL 1 (Sertão - Carcará)
+# ==========================================
+ARQUIVO_FUNDO_1 = "fundo.jpeg"
+ARQUIVO_CHAO_1 = "chao.jpeg"
+ARQUIVO_MUSICA_1 = "musica_sertao.mp3"
+ARQUIVO_PASSARO1_N1 = "carcara.png"
+ARQUIVO_PASSARO2_N1 = "carcara2.png"
+ARQUIVO_OBSTACULO_1 = "cano1.png"  # Nomeie sua imagem do obstáculo 1 assim
+
+# ==========================================
+# ASSETS NÍVEL 2 (Cidade - Pomba)
+# ==========================================
+ARQUIVO_FUNDO_2 = "Copilot_20260625_192002.png"
+ARQUIVO_CHAO_2 = "chao2.png"
+ARQUIVO_MUSICA_2 = "musica2.mp3"
+ARQUIVO_PASSARO1_N2 = "pomba1.png"
+ARQUIVO_PASSARO2_N2 = "pomba2.png"
+ARQUIVO_OBSTACULO_2 = "cano2.png"  # Nomeie sua imagem do obstáculo 2 assim
+
+# ==========================================
+# ASSETS NÍVEL 3 (Caatinga - Acauã)
+# ==========================================
+ARQUIVO_FUNDO_3 = "Copilot_20260625_191901.png"
+ARQUIVO_CHAO_3 = "chao3.jpeg"
+ARQUIVO_MUSICA_3 = "musica3.mp3"
+ARQUIVO_PASSARO1_N3 = "acaua1.png"
+ARQUIVO_PASSARO2_N3 = "acaua2.png"
+ARQUIVO_OBSTACULO_3 = "cano3.png"
+
+# ==========================================
+# ASSETS NÍVEL 4 (Céu Profundo - Gavião)
+# ==========================================
+ARQUIVO_FUNDO_4 = "Copilot_20260625_191956.png"
+ARQUIVO_CHAO_4 = "chao4.png"
+ARQUIVO_MUSICA_4 = "musica4.mp3"
+ARQUIVO_PASSARO1_N4 = "gaviao1.png"
+ARQUIVO_PASSARO2_N4 = "gaviao2.png"
+ARQUIVO_OBSTACULO_4 = "cano4.png"
